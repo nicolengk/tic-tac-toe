b4d1fce223de68cccecdaeb2b13c0c0287215c7a
nicolengk <nicol3slaw@gmail.com>
2023-12-22 20:10:46 +0800

    First commit

---
+++ src/Components/TicTacToe/TicTacToe.jsx
@@ -0,0 +1,53 @@
+import React, { useState } from "react";
+import "./TicTacToe.css";
+import cat_1 from "../Assets/catwithinvisiblebg.jpg";
+import cat_2 from "../Assets/catwithinvisiblebg2.jpg";
+
+let data = ["", "", "", "", "", "", "", "", ""];
+
+const TicTacToe = () => {
+
+  let [count, setCount] = useState(0);
+  let [lock, setLock] = useState;
+
+  const toggle = (w, num) => {
+    if (lock) {
+      return 0;
+    }
+    if (count % 2 === 0) {
+      e.target.innerHTML = '<img src = '${cat_1}'>';
+      data[num] = 1;
+      setCount(++count);
+    } else {
+      e.target.innerHTML = '<img src = '${cat_2}'>';
+      data[num] = 2;
+      setCount(++count);
+    }
+  }
+
+  return (
+    <div className="container">
+      <h1 className="title">Tic Tac Toe</h1>
+      <div className="board">
+        <div className="row1">
+          <div className="boxes"></div>
+          <div className="boxes"></div>
+          <div className="boxes"></div>
+        </div>
+        <div className="row2">
+          <div className="boxes"></div>
+          <div className="boxes"></div>
+          <div className="boxes"></div>
+        </div>
+        <div className="row3">
+          <div className="boxes"></div>
+          <div className="boxes"></div>
+          <div className="boxes"></div>
+        </div>
+      </div>
+      <button className="reset">Reset</button>
+    </div>
+  );
+};
+
+export default TicTacToe;
