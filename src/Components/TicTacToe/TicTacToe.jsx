4b58b250a4a4474219b9c76d49527547d994b854
nicolengk <nicol3slaw@gmail.com>
2023-12-22 20:32:45 +0800

    add on click

--- src/Components/TicTacToe/TicTacToe.jsx
+++ src/Components/TicTacToe/TicTacToe.jsx
@@ -6,43 +6,87 @@ import cat_2 from "../Assets/catwithinvisiblebg2.jpg";
 let data = ["", "", "", "", "", "", "", "", ""];
 
 const TicTacToe = () => {
-
   let [count, setCount] = useState(0);
   let [lock, setLock] = useState;
 
-  const toggle = (w, num) => {
+  const toggle = (e, num) => {
     if (lock) {
       return 0;
     }
     if (count % 2 === 0) {
-      e.target.innerHTML = '<img src = '${cat_1}'>';
-      data[num] = 1;
+      e.target.innerHTML = `<img src = '${cat_1}'>`;
+      data[num] = "1";
       setCount(++count);
     } else {
-      e.target.innerHTML = '<img src = '${cat_2}'>';
-      data[num] = 2;
+      e.target.innerHTML = `<img src = '${cat_2}'>`;
+      data[num] = "2";
       setCount(++count);
     }
-  }
+  };
 
   return (
     <div className="container">
       <h1 className="title">Tic Tac Toe</h1>
       <div className="board">
         <div className="row1">
-          <div className="boxes"></div>
-          <div className="boxes"></div>
-          <div className="boxes"></div>
+          <div
+            className="boxes"
+            onClick={(e) => {
+              toggle(e, 0);
+            }}
+          ></div>
+          <div
+            className="boxes"
+            onClick={(e) => {
+              toggle(e, 1);
+            }}
+          ></div>
+          <div
+            className="boxes"
+            onClick={(e) => {
+              toggle(e, 2);
+            }}
+          ></div>
         </div>
         <div className="row2">
-          <div className="boxes"></div>
-          <div className="boxes"></div>
-          <div className="boxes"></div>
+          <div
+            className="boxes"
+            onClick={(e) => {
+              toggle(e, 3);
+            }}
+          ></div>
+          <div
+            className="boxes"
+            onClick={(e) => {
+              toggle(e, 4);
+            }}
+          ></div>
+          <div
+            className="boxes"
+            onClick={(e) => {
+              toggle(e, 5);
+            }}
+          ></div>
         </div>
         <div className="row3">
-          <div className="boxes"></div>
-          <div className="boxes"></div>
-          <div className="boxes"></div>
+          <div
+            className="boxes"
+            onClick={(e) => {
+              toggle(e, 6);
+            }}
+          ></div>
+          <div
+            className="boxes"
+            onClick={(e) => {
+              toggle(e, 7);
+            }}
+          ></div>
+          <div
+            className="boxes"
+            onClick={(e) => {
+              toggle(e, 8);
+            }}
+          ></div>
         </div>
       </div>
       <button className="reset">Reset</button>
